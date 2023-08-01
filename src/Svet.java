a96cefdb8afe23aa7c0e12fef1f6e3ed6b5d43cb
alkaz07 <>
2023-08-01 13:26:17 +0300

    теперь исходные данные читаются из файла и результат пишется в файл

--- src/Svet.java
+++ src/Svet.java
@@ -1,23 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Svet {
     public static void main(String[] args) {
-        System.out.println("введите N и M:");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] result = new int[n];
+        String fname1 = "INPUT.txt";
+        String fname2 = "OUTPUT.txt";
+       // System.out.println("введите N и M:");
+        try {
+            Scanner sc = new Scanner(new File(fname1));
 
-        System.out.println("введите "+m+"пар чисел");
-        for (int i = 0; i < m; i++) {
-            int perekrestok = sc.nextInt();
-            int k = perekrestok-1;      //потому что в массиве нумерация с 0
-            result[k]++;                //добавляем светофор
-        }
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int[] result = new int[n];
 
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i]+" ");
+          //  System.out.println("введите " + m + "пар чисел");
+            for (int i = 0; i < 2*m; i++) {
+                int perekrestok = sc.nextInt();
+                int k = perekrestok - 1;      //потому что в массиве нумерация с 0
+                result[k]++;                //добавляем светофор
+            }
+
+            FileWriter output = new FileWriter(new File(fname2));
+            for (int i = 0; i < result.length; i++) {
+                //System.out.print(result[i] + " ");
+                output.write(result[i] + " ");
+            }
+           // System.out.println();
+            output.close();
         }
-        System.out.println();
+        catch (FileNotFoundException e)
+        {
+            System.out.println("ойой");
+        } catch (IOException e) {
+            System.out.println("ой-ой-ой");
+        }
     }
 }
